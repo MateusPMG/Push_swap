95c0de38bd8edf8713aa7f69988bb87735ecc0fd
MateusPMG <you@example.com>
2023-01-10 15:11:16 +0000

    input check done

--- input_check.c
+++ input_check.c
@@ -6,7 +6,7 @@
 /*   By: mpatrao <mpatrao@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 13:11:43 by mpatrao           #+#    #+#             */
-/*   Updated: 2023/01/10 13:37:10 by mpatrao          ###   ########.fr       */
+/*   Updated: 2023/01/10 15:04:45 by mpatrao          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,47 @@ int	arg_is_number(char *av)
 {
 	int	i;
 
-	if (is_sign(av[i]) && )
-	{
-
-	}
+	if (is_sign(av[i]) && av[i + 1] != '\0')
+		i++;
+	while (av[i] && is_digit(av[i]))
+		i++;
+	if (av[i] != '\0' && !is_digit(av[i]))
+		return (0);
+	return (1);
 }
 
-int	arg_is_duplicate(char *av)
+int	arg_has_duplicate(char **av)
 {
+	int	i;
+	int	j;
 
+	i = 0;
+	while (av[i])
+	{
+		j = 0;
+		while (av[j])
+		{
+			if (i != j && n_string_compare(av[i], av[j]) == 0)
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
 }
 
 int	arg_is_zero(char *av)
 {
+	int	i;
 
+	i = 0;
+	if (is_sign(av[i]))
+		i++;
+	while (av[i] && av[i] == '0')
+		i++;
+	if (av[i] != '\0')
+		return (0);
+	return (1);
 }
 
 int	valid_input(char **av)
@@ -46,7 +73,7 @@ int	valid_input(char **av)
 	}
 	if (zeros > 1)
 		return (0);
-	if (arg_is_duplicate(av))
+	if (arg_has_duplicate(av))
 		return (0);
 	return (1);
 }
