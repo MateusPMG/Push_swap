df5639811054d6bbb0933105e4e17b4881fd34d3
MateusPMG <you@example.com>
2023-01-10 14:02:17 +0000

    working on input

--- input_check.c
+++ input_check.c
@@ -1,25 +1,52 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   input_check.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mpatrao <mpatrao@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/05 14:31:27 by mpatrao           #+#    #+#             */
-/*   Updated: 2023/01/10 13:20:25 by mpatrao          ###   ########.fr       */
+/*   Created: 2023/01/10 13:11:43 by mpatrao           #+#    #+#             */
+/*   Updated: 2023/01/10 13:37:10 by mpatrao          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(int ac, char **av)
+int	arg_is_number(char *av)
 {
-	t_stack	*stack_a;
-	t_stack	*stack_b;
+	int	i;
 
-	if (ac < 2)
+	if (is_sign(av[i]) && )
+	{
+
+	}
+}
+
+int	arg_is_duplicate(char *av)
+{
+
+}
+
+int	arg_is_zero(char *av)
+{
+
+}
+
+int	valid_input(char **av)
+{
+	int	i;
+	int	zeros;
+
+	while (av[i])
+	{
+		if (!arg_is_number(av[i]))
+			return (0);
+		zeros += arg_is_zero(av[i]);
+		i++;
+	}
+	if (zeros > 1)
 		return (0);
-	if (!valid_input(av))
-		ft_error();
-	
+	if (arg_is_duplicate(av))
+		return (0);
+	return (1);
 }
---
+++ input_check_utils.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   input_check_utils.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpatrao <mpatrao@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/10 13:37:55 by mpatrao           #+#    #+#             */
+/*   Updated: 2023/01/10 13:57:15 by mpatrao          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+int	is_sign(char c)
+{
+	if (c == '-' || c == '+')
+		return (1);
+	return (0);
+}
+
+int	n_string_compare(const	char *s1, const char *s2)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = i;
+	if (s1[i] == '+')
+	{
+		if (s2[j] != '+')
+			i++;
+	}
+	else
+	{
+		if (s2[j] == '+')
+			j++;
+	}
+	while (s1[i] && s2[j] && s1[i] == s2[j])
+	{
+		i++;
+		j++;
+	}
+	return ((unsigned char)s1[i] - (unsigned char)s2[j]);
+}
