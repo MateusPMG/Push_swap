6c63d7c5b92df5dce5a1c6e74d4485e54229b13b
MateusPMG <you@example.com>
2023-01-12 12:38:53 +0000

    going

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: mpatrao <mpatrao@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 14:31:27 by mpatrao           #+#    #+#             */
-/*   Updated: 2023/01/12 10:49:03 by mpatrao          ###   ########.fr       */
+/*   Updated: 2023/01/12 12:38:25 by mpatrao          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int	main(int ac, char **av)
 {
 	t_stack	*stack_a;
 	t_stack	*stack_b;
+	int		size;
 
 	if (ac < 2)
 		return (0);
@@ -23,5 +24,6 @@ int	main(int ac, char **av)
 		ft_error();
 	stack_b = NULL;
 	stack_a = fill_stack(ac, av);
+	size = stack_size(stack_a);
 
 }
