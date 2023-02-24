d6be33ec1f9ea10810a5b88f87248a96dacc1d83
MateusPMG <mpmgboss@gmail.com>
2023-02-24 16:10:02 +0000

    anda

--- get_small_or_big.c
+++ get_small_or_big.c
@@ -6,7 +6,7 @@
 /*   By: mpatrao <mpatrao@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 15:44:10 by mpatrao           #+#    #+#             */
-/*   Updated: 2023/02/24 15:47:20 by mpatrao          ###   ########.fr       */
+/*   Updated: 2023/02/24 16:09:39 by mpatrao          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,5 +30,41 @@ int	get_smallest(t_stack **stack)
 
 int	get_second_smallest(t_stack **stack)
 {
-	
+	long int	min;
+	long int	min2;
+	t_stack		*tmp;
+	int			flag;
+
+	min = get_smallest(stack);
+	min2 = min;
+	tmp = *stack;
+	flag = 0;
+	while (tmp->next != NULL)
+	{
+		if (tmp->value > min && flag == 0)
+		{
+			min2 = tmp->value;
+			flag++;
+		}
+		if (tmp->value > min && tmp->value < min2)
+			min2 = tmp->value;
+		tmp = tmp->next;
+	}
+	return (min2);
+}
+
+int	get_biggest(t_stack **stack)
+{
+	long int	max;
+	t_stack		*tmp;
+
+	max = (*stack)->value;
+	tmp = *stack;
+	while (tmp->next != NULL)
+	{
+		if (tmp->value > max)
+			max = tmp->value;
+		tmp = tmp->next;
+	}
+	return (max);
 }
--- sort_5_or_4.c
+++ sort_5_or_4.c
@@ -6,7 +6,7 @@
 /*   By: mpatrao <mpatrao@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 15:31:21 by mpatrao           #+#    #+#             */
-/*   Updated: 2023/02/24 15:42:22 by mpatrao          ###   ########.fr       */
+/*   Updated: 2023/02/24 16:06:24 by mpatrao          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,17 @@
 void	sort_4(t_stack	**stack_a, t_stack **stack_b)
 {
 	long int	a;
+	long int	b;
 
 	a = get_smallest(stack_a);
+	b = get_second_smallest(stack_a);
 	if (a == (*stack_a)->value)
 	{
 		use_pb(stack_a, stack_b);
 		sort_3(stack_a);
 		use_pa(stack_b, stack_a);
 	}
-	if (a == )
+	if (b == (*stack_a)->value)
+	{
+	}
 }
