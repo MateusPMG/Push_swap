446c073f6ffdec854f186869c42ccd9f68e97a5a
MateusPMG <mpmgboss@gmail.com>
2023-03-02 18:07:22 +0000

    progress

--- algorithm.c
+++ algorithm.c
@@ -6,7 +6,7 @@
 /*   By: mpatrao <mpatrao@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 15:06:44 by mpatrao           #+#    #+#             */
-/*   Updated: 2023/02/28 16:38:21 by mpatrao          ###   ########.fr       */
+/*   Updated: 2023/03/02 18:04:15 by mpatrao          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,33 @@ void	keep_5(t_stack **stack_a, t_stack **stack_b)
 	size = stack_size(*stack_a);
 	pushed = 0;
 	i = 0;
-	while ((size - pushed) != 5)
+	while (size > 5 && pushed < size / 2 && i < size)
 	{
-		
+		if ((*stack_a)->position <= size / 2)
+		{
+			use_pb(stack_a, stack_b);
+			pushed++;
+		}
+		else
+			use_ra(stack_a);
+		i++;
+	}
+	while (size - pushed > 5)
+	{
+		use_pb(stack_a, stack_b);
+		pushed++;
 	}
 }
 
 void	algorithm(t_stack **stack_a, t_stack **stack_b)
 {
+	t_stack	*tmp;
+
 	keep_5(stack_a, stack_b);
 	sort_5(stack_a, stack_b);
-	
+	while (*stack_b)
+	{
+		tmp = min_cost_adress(*stack_a, *stack_b);
+		do_moves(tmp);
+	}
 }
--- cost.c
+++ cost.c
@@ -6,37 +6,70 @@
 /*   By: mpatrao <mpatrao@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 14:30:51 by mpatrao           #+#    #+#             */
-/*   Updated: 2023/03/01 15:06:59 by mpatrao          ###   ########.fr       */
+/*   Updated: 2023/03/02 18:07:12 by mpatrao          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	current_cost(t_stack *stack)
+int	current_cost(t_stack *stack_a, t_stack *stack_b)
 {
 	int		cost;
-	t_stack	*tmp;
 
-	tmp = stack;
-	cost = 0;
-	while (tmp)
-	{
-		
-	}
-
+	cost = get_cost_b(stack_b) + 1 + get_cost_a(stack_a, stack_b);
 	return (cost);
 }
 
-int	min_cost(t_stack *stack)
+int	get_cost_b(t_stack *stack_b)
+{
+	int		i;
+	int		cost_b;
+	int		size;
+	t_stack	*tmp_b;
+
+	tmp_b = stack_b;
+	size = stack_size(stack_b);
+	i = 0;
+	while (tmp_b)
+	{
+		tmp_b = tmp_b->next;
+		i++;
+	}
+	if (i > size / 2)
+		cost_b = size - i;
+	else if (i <= size / 2)
+		cost_b = i;
+	return (cost_b);
+}
+
+int	get_cost_a(t_stack *stack_a, t_stack *stack_b)
+{
+	int		i;
+	int		cost_a;
+	int		size;
+	t_stack	*tmp_a;
+
+	tmp_a = stack_a;
+	
+}
+
+t_stack	*min_cost_adress(t_stack *stack_a, t_stack *stack_b)
 {
 	int		cost;
+	t_stack	*adress_b;
 	t_stack	*tmp;
 
-	tmp = stack;
+	tmp = stack_b->next;
+	cost = current_cost(stack_a, stack_b);
+	adress_b = stack_b;
 	while (tmp)
 	{
-		if (cost < current_cost(tmp))
-			cost = current_cost(tmp);
-		
+		if (cost < current_cost(stack_a, tmp))
+		{
+			cost = current_cost(stack_a, tmp);
+			adress_b = tmp;
+		}
+		tmp = tmp->next;
 	}
+	return (adress_b);
 }
